f38e8f8148eaa1f23952dbb25d172ddca3958fb1
drkohlipk <drkohlipk@yahoo.com>
2016-10-21 21:48:14 -0700

    Fun with JQuery

--- JQuery/JQuery_Functions_Assignment1/JQuery_Functions.js
+++ JQuery/JQuery_Functions_Assignment1/JQuery_Functions.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	var $mainh2 = $('#mainCard h2'),
 		$mainBtn = $('#mainCard button'),
-		$mainP = $('#mainCard p');
+		$mainP = $('#mainCard p:nth-child(3)'),
+		counter = 0;
 
 	/********Moves to the 2nd slide********/
 	$('#start').click(function() {
@@ -10,6 +11,21 @@ $(document).ready(function() {
 	});
 
 	$mainBtn.click(function() {
-
+		if (counter === 0) {
+			$mainP.addClass('redify');
+			$mainh2.text('Get Ready!');
+			$mainP.text(
+				'Add .addClass and .text to the list!  Wooooooo!!');
+		} else if (counter == 1) {
+			$mainP.removeClass('redify');
+			$mainh2.text('HERE IT COMES!')
+			$mainP.text(
+				"Alright, enough of the child's play, let's do something awesome...");
+		} else if (counter == 2) {
+			$('#mainCard').html(
+				"<h1>BACON!!!</h1><img src='http://big.assets.huffingtonpost.com/slide_297900_2459978_free.gif' alt='Sizzling Bacon' /><button>Next</button>"
+			);
+		}
+		counter++;
 	});
 });
