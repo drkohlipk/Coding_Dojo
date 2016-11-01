8f83ff43e3aed8539b01baa05c0e48c65465fbc3
drkohlipk <drkohlipk@yahoo.com>
2016-10-31 17:38:27 -0700

    Second part of Selection Sort completed

--- Python/12_Selection_Sort/selection_sort.py
+++ Python/12_Selection_Sort/selection_sort.py
@@ -8,7 +8,7 @@ def populate():
 		randos.append(random.randint(0,10000)) #append random values to the randos list
 	return randos
 
-##########sort the randomly populated list#########
+##########sort the randomly populated list...get min only#########
 def sel_sort(arr):
 	temp, idx, count = 0, 0, 0
 	for i in range(0, len(arr)): #for the length of arr...
@@ -23,6 +23,28 @@ def sel_sort(arr):
 		arr[i] = arr[idx]
 		arr[idx] = temp
 	return arr, count
+
+##########sort the randomly populated list...get min and max#########
+# def sel_sort(arr):
+# 	temp, idxMin, idxMax, count, currj = 0, 0, 0, 0, 0
+# 	for i in range(0, len(arr)): #for the length of arr...
+# 		idxMin = i
+# 		idxMax = i
+# 		for j in range(i, len(arr)): #iterate through arr...
+# 			currj = j
+# 			if arr[j] < arr[idxMin]: #if the current value is less than the current minimum...
+# 				idxMin = j #set idx to the index of the current minimum
+# 				count += 1 #count if else!
+# 			elif arr[j] > arr[idxMax]: #if the current value is greater than the current maximum...
+# 				idxMax = j #set idx to the index of the current maximum
+# 				count += 1 #count if else!
+# 		temp = arr[i] #swap arr at index of i with the found minimum.
+# 		arr[i] = arr[idxMin]
+# 		arr[idxMin] = temp
+# 		temp = arr[currj] #swap the last value of arr with the found maximum.
+# 		arr[currj] = arr[idxMax]
+# 		arr[idxMax] = temp
+# 	return arr, count
 
 new_Array = populate() #make your random list
 a = datetime.datetime.now() #start the clock!
