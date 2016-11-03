7a40fa9a2b6d34a652ba6e058c16d61931694b68
drkohlipk <drkohlipk@yahoo.com>
2016-11-03 15:44:31 -0700

    Completed Registration Form Assignment

--- Javascript/play.js
+++ Javascript/play.js
@@ -39,26 +39,72 @@
 //
 // console.log(newString2);
 //
-function reverseString(str) {
-	newString = "";
-	for (var i = str.length - 1; i >= 0; i--) {
-		newString += str[i];
+// function reverseString(str) {
+// 	newString = "";
+// 	for (var i = str.length - 1; i >= 0; i--) {
+// 		newString += str[i];
+// 	}
+// 	return newString;
+// }
+//
+// console.log(reverseString("Reverse Me!"));
+//
+// newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//
+// function reverseArray(arr) {
+// 	temp = 0;
+// 	for (var i = 0; i < Math.floor(arr.length / 2); i++) {
+// 		temp = arr[i];
+// 		arr[i] = arr[arr.length - 1 - i];
+// 		arr[arr.length - 1 - i] = temp;
+// 	}
+// }
+//
+// reverseArray(newArray);
+// console.log(newArray);
+//
+
+var people = [{
+	name: "Robert",
+	pet_id: 2
+}, {
+	name: "Swetha",
+	pet_id: 4
+}, {
+	name: "Jack",
+	pet_id: 3
+}, {
+	name: "Bob",
+	pet_id: 1
+}];
+
+var pets = [{
+	id: 1,
+	type: 'fish'
+}, {
+	id: 2,
+	type: 'dog'
+}, {
+	id: 3,
+	type: 'cat'
+}, {
+	id: 4,
+	type: 'pygmy elephant'
+}];
+
+function joinByPet_id(arr1, arr2) {
+	var array = [];
+	for (var i = 0; i < arr1.length; i++) {
+		var obj = {};
+		obj.name = arr1[i].name;
+		for (var j = 0; j < arr2.length; j++) {
+			if (arr1[i].pet_id == arr2[j].id) {
+				obj.pet = arr2[j].type;
+			}
+		}
+		array.push(obj);
 	}
-	return newString;
+	return array;
 }
 
-console.log(reverseString("Reverse Me!"));
-
-newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-function reverseArray(arr) {
-	temp = 0;
-	for (var i = 0; i < Math.floor(arr.length / 2); i++) {
-		temp = arr[i];
-		arr[i] = arr[arr.length - 1 - i];
-		arr[arr.length - 1 - i] = temp;
-	}
-}
-
-reverseArray(newArray);
-console.log(newArray);
+console.log(joinByPet_id(people, pets));
