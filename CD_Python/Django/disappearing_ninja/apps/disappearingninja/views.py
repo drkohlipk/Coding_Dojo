1826ccb789e8aec986c263142ebfaf8ca4e39223
drkohlipk <drkohlipk@yahoo.com>
2016-11-10 13:36:00 -0800

    Initial set up disappearing ninja

--- CD_Python/Django/disappearing_ninja/apps/disappearingninja/views.py
+++ CD_Python/Django/disappearing_ninja/apps/disappearingninja/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'disappearingninja/index.html')
+
+def ninjas(request):
+    try:
+        context = {
+            'label' : request.session['label'],
+            'img' : request.session['img']
+        }
+    except KeyError:
+        request.session['label'] = 'placeholder'
+        request.session['img'] = 'placeholder'
+        context = {
+            'label' : request.session['label'],
+            'img' : request.session['img']
+        }
+    return render(request, 'disappearingninja/ninjas.html', context)
+
+def processninjas(request, id):
+    return redirect('/ninjas')
