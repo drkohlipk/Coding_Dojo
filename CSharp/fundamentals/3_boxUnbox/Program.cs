5c6f13c64696b0f14a9d79559f10f0a1c5e42e71
drkohlipk <drkohlipk@yahoo.com>
2016-11-27 19:55:03 -0800

    Completed assignment 3, box, unbox for C Sharp

---
+++ CSharp/fundamentals/3_boxUnbox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            List<object> box = new List<object>();
+            box.Add(7);
+            box.Add(28);
+            box.Add(-1);
+            box.Add(true);
+            foreach (var item in box)
+            {
+                if (item is int)
+                {
+                    int tempitem = (int)item;
+                    sum += tempitem;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
