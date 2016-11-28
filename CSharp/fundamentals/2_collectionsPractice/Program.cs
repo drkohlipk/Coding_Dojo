e49ca97f103215e83e91ade16f9d62485096f618
drkohlipk <drkohlipk@yahoo.com>
2016-11-27 19:13:31 -0800

    Accidently deleted a few things.  Everything is fixed now

--- CSharp/fundamentals/2_collectionsPractice/Program.cs
+++ CSharp/fundamentals/2_collectionsPractice/Program.cs
@@ -1,4 +1,21 @@
-﻿// 
+﻿// using System;
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int[] arr1 = new int[10];
+//             string[] arr2 = {"Tim", "Martin", "Nikki", "Sara"};
+//             bool[] arr3 = new bool[10];
+//             for (int i = 0; i < 10; i += 2)
+//             {
+//                 arr3[i] = true;
+//             }
+//         }
+//     }
+// }
 
 // using System;
 
