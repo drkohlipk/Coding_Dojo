903d09ab33817e65d84c1c4402c76f88afd254e6
drkohlipk <drkohlipk@yahoo.com>
2017-03-18 22:47:55 -0700

    Complete with basic SLL algos for CtCI

--- Algorithms/16_mar/helpers16mar/SList.java
+++ Algorithms/16_mar/helpers16mar/SList.java
@@ -1,36 +1,46 @@
 package helpers16mar;
 
-import static java.lang.System.out;
+import java.lang.*;
 
 public class SList {
 	Node head;
+	Node tail;
 
 	public SList() {
 		this.head = null;
+		this.tail = null;
 	}
 
 	public void addFront(int val) {
 		Node tempNode = new Node(val);
 		tempNode.next = this.head;
+		if (this.head == null) this.tail = tempNode;
 		this.head = tempNode;
+	}
+
+	public void addBack(int val) {
+		Node tempNode = new Node(val);
+		if (this.head == null) this.head = tempNode;
+		else this.tail.next = tempNode;
+		this.tail = tempNode;
 	}
 
 	public void printList() {
 		if (this.head == null) {
-			throw new java.lang.IllegalArgumentException("List empty");
+			throw new IllegalArgumentException("List empty");
 		}
 
 		Node currNode = this.head;
 
 		while (currNode != null) {
-			out.println(currNode.value);
+			System.out.println(currNode.value);
 			currNode = currNode.next;
 		}
 	}
 
 	public Node findNode(int val) {
 		if (this.head == null) {
-			throw new java.lang.IllegalArgumentException("List empty");
+			throw new IllegalArgumentException("List empty");
 		}
 
 		Node currNode = this.head;
@@ -40,13 +50,13 @@ public class SList {
 			currNode = currNode.next;
 		}
 
-		out.println("Node not in list");
+		System.out.println("Node not in list");
 		return null;
 	}
 
 	public void remKnownNode(Node delNode) {
 		if (delNode == null || delNode.next == null) {
-			throw new java.lang.IllegalArgumentException("Node passed is either null or the last node");
+			throw new IllegalArgumentException("Node passed is either null or the last node");
 		}
 
 		delNode.value = delNode.next.value;
@@ -55,7 +65,7 @@ public class SList {
 
 	public void swapVals(Node n1, Node n2) {
 		if (n1 == null || n2 == null) {
-			throw new java.lang.IllegalArgumentException("One or both nodes passed are null");
+			throw new IllegalArgumentException("One or both nodes passed are null");
 		}
 
 		int temp = n1.value;
@@ -65,7 +75,7 @@ public class SList {
 
 	public void partition(int partVal) {
 		if (this.head == null) {
-			throw new java.lang.IllegalArgumentException("List empty");
+			throw new IllegalArgumentException("List empty");
 		}
 
 		Node bigNode = this.head;
@@ -76,5 +86,36 @@ public class SList {
 			if (bigNode.value < partVal) bigNode = bigNode.next;
 			if (smallNode.value >= partVal) smallNode = smallNode.next;
 		}
+	}
+
+	public void makeList(int num) {
+		while (num > 0) {
+			int currNumber = num;
+			num /= 10;
+			this.addBack(currNumber - (num * 10));
+		}
+	}
+
+	public int getLength() {
+		int count = 0;
+		Node currNode = this.head;
+		while (currNode != null) {
+			count++;
+			currNode = currNode.next;
+		}
+		return count;
+	}
+
+	public int getNum(int length) {
+		int power = 0,
+			currNum = 0;
+		Node currNode = this.head;
+		while (power < length) {
+			int multiplier = (int) Math.pow(10, power++);
+			currNum += currNode.value * multiplier;
+			currNode = currNode.next;
+		}
+		if (currNode != null) throw new IllegalArgumentException("Length provided is not big enough");
+		return currNum;
 	}
 }
